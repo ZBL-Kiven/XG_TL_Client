6d81d8084a63c10b9ac7667003100d1adec4f397
ZJJ <zjj0888@gmail.com>
2023-03-13 16:49:06 +0800

    国家称号配置器

--- src/Interface/GameSetup/GameSetup.lua
+++ src/Interface/GameSetup/GameSetup.lua
@@ -1,49 +1,45 @@
 local g_PreAlpha = "0.8";
 g_NewNewMenu = 1
+local isShowingCountryTitle = 0
 --===============================================
 -- PreLoad()
 --===============================================
 function GameSetup_PreLoad()
-
 	this:RegisterEvent("TOGLE_GAMESETUP");
-
+	this:RegisterEvent("UI_COMMAND");
 	this:RegisterEvent("TOGLE_SYSTEMFRAME");
 	this:RegisterEvent("TOGLE_SOUNDSETUP");
 	this:RegisterEvent("TOGLE_VIEWSETUP");
 	this:RegisterEvent("PLAYER_ENTERING_WORLD");
-	local n1,n2,n3,n4,n5,n6,n7,n8,n9,f10,n11,n12,n13,n14,n15 = SystemSetup:GameGetData();
-	SystemSetup:SaveGameSetup ( n1,n2,n3,n4,n5,n6,n7,1,n9,tonumber(f10),n11,n12,1,n14,0 );
+	local n1, n2, n3, n4, n5, n6, n7, n8, n9, f10, n11, n12, n13, n14, n15 = SystemSetup:GameGetData();
+	SystemSetup:SaveGameSetup(n1, n2, n3, n4, n5, n6, n7, 1, n9, tonumber(f10), n11, n12, 1, n14, 0);
 end
 
 --===============================================
 -- OnLoad()
 --===============================================
 function GameSetup_OnLoad()
-	GameSetup_ChatBkg_Slider:SetProperty( "DocumentSize","1" );
-	GameSetup_ChatBkg_Slider:SetProperty( "PageSize","0.1" );
-	GameSetup_ChatBkg_Slider:SetProperty( "StepSize","0.1" );
+	GameSetup_ChatBkg_Slider:SetProperty("DocumentSize", "1");
+	GameSetup_ChatBkg_Slider:SetProperty("PageSize", "0.1");
+	GameSetup_ChatBkg_Slider:SetProperty("StepSize", "0.1");
 end
 
 --===============================================
 -- OnEvent()
 --===============================================
 function GameSetup_OnEvent(event)
-	
-	if ( event == "TOGLE_GAMESETUP" ) then
+	if (event == "TOGLE_GAMESETUP") then
 		this:Show();
-		local old = {SystemSetup:GameGetData()};
+		local old = { SystemSetup:GameGetData() };
 		g_PreAlpha = tostring(old[10]);
 		GameSetup_UpdateFrame();
-
-	elseif(event == "TOGLE_VIEWSETUP" and this:IsVisible()) then
+	elseif (event == "TOGLE_VIEWSETUP" and this:IsVisible()) then
 		GameSetup_Cancel_Clicked();
-
-	elseif(event == "TOGLE_SYSTEMFRAME" and this:IsVisible()) then
+	elseif (event == "TOGLE_SYSTEMFRAME" and this:IsVisible()) then
 		GameSetup_Cancel_Clicked();
-
-	elseif(event == "TOGLE_SOUNDSETUP" and this:IsVisible()) then
+	elseif (event == "TOGLE_SOUNDSETUP" and this:IsVisible()) then
 		GameSetup_Cancel_Clicked();
-	elseif(event == "PLAYER_ENTERING_WORLD" ) then
+	elseif (event == "PLAYER_ENTERING_WORLD") then
 		GameSetup_NewMenu:SetCheck(1)
 		GameSetup_AutoFollow:SetCheck(1)
 		GameSetup_CoolDown:SetCheck(1)
@@ -54,7 +50,6 @@ function GameSetup_OnEvent(event)
 		--技能倒计时
 		GameSetup_GameSetup_CoolDown()
 	end
-
 end
 
 
@@ -62,23 +57,23 @@ end
 -- UpdateFrame()
 --===============================================
 function GameSetup_UpdateFrame()
-
-	local n1,n2,n3,n4,n5,n6,n7,n8,n9,f10,n11,n12,n13,n14,n15 = SystemSetup:GameGetData();
-	
-	GameSetup_Item1						:SetCheck(n1);					-- 拒绝所有信件
-	GameSetup_Item2						:SetCheck(n2);					-- 拒绝加我好友
-	GameSetup_Item3						:SetCheck(n3);					-- 拒绝默认生信件
-	GameSetup_Item4						:SetCheck(n4);					-- 拒绝交易
-	GameSetup_Item5						:SetCheck(n5);					-- 拒绝队伍邀请
-	GameSetup_Item6						:SetCheck(n6);					-- 关闭当前泡泡框
-	GameSetup_Item7						:SetCheck(n7);					-- 拒绝查看配偶
-	GameSetup_Item8						:SetCheck(n8);					-- 角色显示帽子
-	GameSetup_Item9						:SetCheck(n9);					-- 非聊天模式
-	GameSetup_ChatBkg_Slider	:SetPosition(f10);			-- 聊天背景透明度
-	GameSetup_Item11					:SetCheck(n11);					-- 关闭快捷栏提示
-	GameSetup_Lock						:SetCheck(n12);					-- 锁定快捷栏
-	GameSetup_Scene						:SetCheck(n13);					-- 快速切换场景
-	GameSetup_ChatItem				:SetCheck(n15);					-- 快捷键查看链接
+	local n1, n2, n3, n4, n5, n6, n7, n8, n9, f10, n11, n12, n13, n14, n15 = SystemSetup:GameGetData();
+	GameSetup_Item1:SetCheck(n1);                    -- 拒绝所有信件
+	GameSetup_Item2:SetCheck(n2);                    -- 拒绝加我好友
+	GameSetup_Item3:SetCheck(n3);                    -- 拒绝陌生人信件
+	GameSetup_Item4:SetCheck(n4);                    -- 拒绝交易
+	GameSetup_Item5:SetCheck(n5);                    -- 拒绝队伍邀请
+	GameSetup_Item6:SetCheck(n6);                    -- 关闭当前泡泡框
+	GameSetup_Item7:SetCheck(n7);                    -- 拒绝查看配偶
+	GameSetup_Item8:SetCheck(n8);                    -- 角色显示帽子
+	GameSetup_Item9:SetCheck(n9);                    -- 非聊天模式
+	GameSetup_ChatBkg_Slider:SetPosition(f10);            -- 聊天背景透明度
+	GameSetup_Item11:SetCheck(n11);                    -- 关闭快捷栏提示
+	GameSetup_Lock:SetCheck(n12);                    -- 锁定快捷栏
+	GameSetup_Scene:SetCheck(n13);                    -- 快速切换场景
+	GameSetup_ChatItem:SetCheck(n15);                    -- 快捷键查看链接
+	isShowingCountryTitle = DataPool:GetPlayerMission_DataRound(215)
+	GameSetup_ACME:SetCheck(isShowingCountryTitle)
 
 end
 
@@ -87,12 +82,10 @@ end
 -- 确定
 --===============================================
 function GameSetup_Accept_Clicked()
-
-	local n1,n2,n3,n4,n5,n6,n7,n8,n9,f10,n11,n12,n13,n14,n15 = SystemSetup:GameGetData();
-
-	n1 = GameSetup_Item1:GetCheck();									-- 拒绝所有信件
+	local n1, n2, n3, n4, n5, n6, n7, n8, n9, f10, n11, n12, n13, n14, n15 = SystemSetup:GameGetData();
+	n1 = GameSetup_Item1:GetCheck();                                    -- 拒绝所有信件
 	n2 = GameSetup_Item2:GetCheck();                  -- 拒绝加我好友       
-	n3 = GameSetup_Item3:GetCheck();                  -- 拒绝默认生信件     
+	n3 = GameSetup_Item3:GetCheck();                  -- 拒绝陌生人信件
 	n4 = GameSetup_Item4:GetCheck();                  -- 拒绝交易           
 	n5 = GameSetup_Item5:GetCheck();                  -- 拒绝队伍邀请       
 	n6 = GameSetup_Item6:GetCheck();                  -- 关闭当前泡泡框     
@@ -104,28 +97,35 @@ function GameSetup_Accept_Clicked()
 	n12 = GameSetup_Lock:GetCheck();                  -- 锁定快捷栏         
 	n13 = GameSetup_Scene:GetCheck();                 -- 快速切换场景       
 	n15 = GameSetup_ChatItem:GetCheck();              -- 快捷键查看链接
-	SystemSetup:SaveGameSetup ( n1,n2,n3,n4,n5,n6,n7,n8,n9,tonumber(f10),n11,n12,n13,n14,n15 );
+	SystemSetup:SaveGameSetup(n1, n2, n3, n4, n5, n6, n7, n8, n9, tonumber(f10), n11, n12, n13, n14, n15);
 	g_PreAlpha = f10;
+	local hideCountryTitle = GameSetup_ACME:GetCheck()
+	if hideCountryTitle ~= isShowingCountryTitle then
+		Clear_XSCRIPT();
+		Set_XSCRIPT_Function_Name("setCountryTitleVisibility");
+		Set_XSCRIPT_ScriptID(2302251);
+		Set_XSCRIPT_Parameter(0, hideCountryTitle)
+		Set_XSCRIPT_ParamCount(1)
+		Send_XSCRIPT();
+	end
 	this:Hide();
 end
 
 function GameSetup_GameSetup_Replacement()
-	local nBit = GameSetup_ACME:GetCheck()
-	LuaFnModelReplacement(nBit)		
 end
 
 function GameSetup_GameSetup_AutoFollow()
-	SetGameMissionData("MD_AUTOFOLLOW",GameSetup_AutoFollow:GetCheck())
+	SetGameMissionData("MD_AUTOFOLLOW", GameSetup_AutoFollow:GetCheck())
 end
 
 function GameSetup_GameSetup_NewMenu()
 	g_NewNewMenu = GameSetup_NewMenu:GetCheck()
 	if g_NewNewMenu == 0 then
 		--通知关闭
-		PushEvent("UI_COMMAND",202209231)
+		PushEvent("UI_COMMAND", 202209231)
 	else
 		--通知开启
-		PushEvent("UI_COMMAND",202209232)
+		PushEvent("UI_COMMAND", 202209232)
 	end
 end
 
@@ -137,9 +137,8 @@ end
 -- 取消
 --===============================================
 function GameSetup_Cancel_Clicked()
-
 	GameSetup_ChatBkg_Slider:SetPosition(g_PreAlpha);
-	Talk:HandleMainBarAction("chatbkg",g_PreAlpha);
+	Talk:HandleMainBarAction("chatbkg", g_PreAlpha);
 	this:Hide();
 
 end
@@ -149,21 +148,20 @@ end
 -- 恢复默认
 --===============================================
 function GameSetup_Default_Clicked()
-
-	GameSetup_Item1						:SetCheck(0);							-- 拒绝所有信件
-	GameSetup_Item2						:SetCheck(0);             -- 拒绝加我好友
-	GameSetup_Item3						:SetCheck(0);             -- 拒绝默认生信件
-	GameSetup_Item4						:SetCheck(0);             -- 拒绝交易
-	GameSetup_Item5						:SetCheck(0);             -- 拒绝队伍邀请
-	GameSetup_Item6						:SetCheck(0);             -- 关闭当前泡泡框
-	GameSetup_Item7						:SetCheck(0);             -- 拒绝查看配偶
-	GameSetup_Item8						:SetCheck(0);             -- 角色显示帽子
-	GameSetup_Item9						:SetCheck(0);             -- 非聊天模式
-	GameSetup_ChatBkg_Slider	:SetPosition(1);       		-- 聊天背景透明度
-	GameSetup_Item11					:SetCheck(1);							-- 关闭快捷栏提示
-	GameSetup_Lock						:SetCheck(0);             -- 锁定快捷栏
-	GameSetup_Scene						:SetCheck(1);             -- 快速切换场景
-	GameSetup_ChatItem				:SetCheck(0);             -- 快捷键查看链接
+	GameSetup_Item1:SetCheck(0);             -- 拒绝所有信件
+	GameSetup_Item2:SetCheck(0);             -- 拒绝加我好友
+	GameSetup_Item3:SetCheck(0);             -- 拒绝陌生人信件
+	GameSetup_Item4:SetCheck(0);             -- 拒绝交易
+	GameSetup_Item5:SetCheck(0);             -- 拒绝队伍邀请
+	GameSetup_Item6:SetCheck(0);             -- 关闭当前泡泡框
+	GameSetup_Item7:SetCheck(0);             -- 拒绝查看配偶
+	GameSetup_Item8:SetCheck(0);             -- 角色显示帽子
+	GameSetup_Item9:SetCheck(0);             -- 非聊天模式
+	GameSetup_ChatBkg_Slider:SetPosition(1);            -- 聊天背景透明度
+	GameSetup_Item11:SetCheck(1);                            -- 关闭快捷栏提示
+	GameSetup_Lock:SetCheck(0);             -- 锁定快捷栏
+	GameSetup_Scene:SetCheck(1);             -- 快速切换场景
+	GameSetup_ChatItem:SetCheck(0);             -- 快捷键查看链接
 
 end
 
@@ -173,6 +171,6 @@ end
 --===============================================
 function GameSetup_ChatBkg_Change()
 	local pos = GameSetup_ChatBkg_Slider:GetPosition();
-
-	Talk:HandleMainBarAction("chatbkg",pos);
+	
+	Talk:HandleMainBarAction("chatbkg", pos);
 end
